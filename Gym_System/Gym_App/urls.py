5034c57621a73c36a7966ee4f0571394bf18a726
TlotloMogami <tlotlomogami@gmail.com>
2025-04-18 16:00:37 +0000

    Base template, Login page upload and Templates Configuration.

--- Gym_System/Gym_App/urls.py
+++ Gym_System/Gym_App/urls.py
@@ -1,6 +1,15 @@
+from django.urls import path
 from . import views
-from django.urls import  path
 
-urlpatterns = [
-    path("", views.loginPage )
+urlpatterns = [ 
+    path("", views.loginPage),
+    path("dashboard/", views.dashboard, name = 'dashboard'),
+    path("members/" , views.members, name='members'),
+    path("trainers/" , views.trainers, name='trainers'),
+    path("instructors/" , views.instructors, name='instructors'),
+    path("general_employees/" , views.general_employees, name='general_employees'),
+    path("reports/" , views.reports, name='reports'),
+    path("register_members/" , views.register_members, name='register_members'),
+    path("login/", views.loginPage ,name='loginpage'),
+    path("class_scheduling/", views.class_scheduling ,name='class_scheduling')
 ]
--- Gym_System/Gym_App/views.py
+++ Gym_System/Gym_App/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, HttpResponse
 
 def loginPage(request):
-    return render(request, "loginPage.html")
+    return render(request, 'loginPage.html')
+
+def dashboard(request):
+    return render(request, 'basetemplate.html' , {'page_title':'dashboard'})
+
+def members(request):
+    return render(request, 'members.html' ,{'page_title':'members'})
+
+def trainers(request):
+    return render(request, 'trainers.html' ,{'page_title':'trainers'})
+
+def instructors(request):
+    return render(request, 'instructors.html' ,{'page_title':'instructors'})
+
+def general_employees(request):
+    return render(request, 'general_employees.html' ,{'page_title':'general_Employees'})
+
+def reports(request):
+    return render(request, 'reports.html' ,{'page_title':'reports'})
+
+def register_members(request):
+    return render(request, 'register_members.html' ,{'page_title':'register Members'})
+
+def class_scheduling(request):
+    return render(request, 'class_scheduling.html' ,{'page_title':'classes'})
